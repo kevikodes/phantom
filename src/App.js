67622da167c5d3d77f9cbe97e18eaa2c277ef80f
Kevin Owens III <krowens3rd@gmail.com>
2022-01-11 11:26:39 -0800

    Add methods for loggin in and out via google.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import "./App.css";
+import { useEffect, useState } from "react";
+import { auth, provider } from "./utils/firebase/firebase.config";
+import { onAuthStateChanged, signInWithPopup } from "firebase/auth";
+import { signUpWithGoogle, handleLogout } from "./utils/firebase/auth";
 
 function App() {
-  return <div className="App"></div>;
+  const [user, setUser] = useState();
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+    return unsub;
+    console.log(user);
+  }, [user]);
+  return (
+    <div className="App">
+      <h1>Firebase</h1>
+      <button>Connect MetaMask Wallet</button>
+      {!user && <button onClick={signUpWithGoogle}>Sign in with google</button>}
+      {user && <button onClick={handleLogout}>Logout</button>}
+    </div>
+  );
 }
 
 export default App;
