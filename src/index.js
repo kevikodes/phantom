b20f894771dc5505691612232d76ce650ee12f68
Kevin Owens III <krowens3rd@gmail.com>
2022-01-11 11:38:34 -0800

    added thirwebprovider

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+//Import thirdweb provider
+import { ThirdwebProvider } from "@3rdweb/react";
+
+const supportedChainIds = [4];
+const connectors = {
+  injected: {},
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThirdwebProvider
+      connectors={connectors}
+      supportedChainIds={supportedChainIds}
+    >
+      <App />
+    </ThirdwebProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
