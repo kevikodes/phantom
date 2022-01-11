506ae0801d51cda666edaf7a4b4f662691495d61
Kevin Owens III <krowens3rd@gmail.com>
2022-01-11 11:56:36 -0800

    added connect wallet functionality

--- src/App.js
+++ src/App.js
@@ -4,29 +4,50 @@ import { auth, provider } from "./utils/firebase/firebase.config";
 import { onAuthStateChanged, signInWithPopup } from "firebase/auth";
 import { signUpWithGoogle, handleLogout } from "./utils/firebase/auth";
 
+//Import thirdweb dependencies
+import { useWeb3 } from "@3rdweb/hooks";
+
 function App() {
-  const [user, setUser] = useState();
+  //useStates
+  const [walletAddress, setWalletAddress] = useState("");
+  const [user, setUser] = useState({});
+
+  //Use thirdweb hook
+  const { address, chainId, connectWallet } = useWeb3();
+
+  //Check for user logged in or out
   useEffect(() => {
     const unsub = onAuthStateChanged(auth, (user) => {
       setUser(user);
     });
-    return unsub;
     console.log(user);
+    return unsub;
   }, [user]);
   return (
     <div className="App">
-      <button
-        style={{ border: "2px solid black", margin: "20px", padding: "10px" }}
-      >
-        Connect MetaMask Wallet
-      </button>
       {!user && (
-        <button
-          style={{ border: "2px solid black", margin: "20px", padding: "10px" }}
-          onClick={signUpWithGoogle}
-        >
-          Sign in with google
-        </button>
+        <>
+          <button
+            style={{
+              border: "2px solid black",
+              margin: "20px",
+              padding: "10px",
+            }}
+            onClick={signUpWithGoogle}
+          >
+            Sign in with google
+          </button>
+          <button
+            style={{
+              border: "2px solid black",
+              margin: "20px",
+              padding: "10px",
+            }}
+            onClick={() => connectWallet("injected")}
+          >
+            Connect MetaMask Wallet
+          </button>
+        </>
       )}
       {user && (
         <button
@@ -35,6 +56,12 @@ function App() {
         >
           Logout
         </button>
+      )}
+
+      {address && user && (
+        <>
+          <div>Address: {address}</div>
+        </>
       )}
     </div>
   );
