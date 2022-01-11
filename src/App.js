5d6f271738a726b0424d97ff8bec6206ff1686b9
Kevin Owens III <krowens3rd@gmail.com>
2022-01-11 10:46:11 -0800

    clean up

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 import "./App.css";
-import { useEffect, useState } from "react";
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "./utils/firebase/auth";
 
 import React from "react";
-import axios from "axios";
 
 const App = () => {
-  const [user, setUser] = useState({});
-
-  useEffect(() => {}, []);
-
   return <div></div>;
 };
 
