54df24cac84a9809eb9b7c1841053b48045e3f2b
Kevin Owens III <krowens3rd@gmail.com>
2022-01-12 07:09:44 -0800

    removed unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,5 @@
 import "./App.css";
-import { useEffect, useState } from "react";
-import { auth, provider } from "./utils/firebase/firebase.config";
-import { onAuthStateChanged, signInWithPopup } from "firebase/auth";
-import { handleAuth, handleLogout } from "./utils/firebase/auth";
 
-//Import thirdweb dependencies
-import { useWeb3 } from "@3rdweb/hooks";
 import ConnectButton from "./components/ConnectButton";
 
 function App() {
