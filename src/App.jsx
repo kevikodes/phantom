b4c3d1332998fdee3a79d4bf7cfa20d1718a9fce
Kevin Owens III <krowens3rd@gmail.com>
2022-01-12 18:59:06 -0800

    add glass and color scheme

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <div className="glass">
+        <h1 className="header">Meta Mask</h1>
+        <button className="button">Connect</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ styles.md
@@ -0,0 +1,11 @@
+60% - #020402
+30% - #c5efcb
+10% - #758173
+
+Colors -
+Backgrounds:
+Primary - #020402
+Secondary - #c5efcb
+
+Text:
+Primary -
