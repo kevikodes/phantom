211af1ad363ad536a99a68d2ff81142ec0ab12db
Kevin Owens III <krowens3rd@gmail.com>
2022-01-16 04:16:16 -0800

    fixed faq container styling

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Faq = ({ question, answer }) => {
   return (
-    <>
+    <div className="faq">
       <div className="faqQuestion">{question}</div>
       <div className="faqAnswer">{answer}</div>
-    </>
+    </div>
   );
 };
 
