edc60613f48f6aca64804061b04afc1abf830a80
Charnice <86922277+ctatum20@users.noreply.github.com>
2022-01-17 13:19:21 -0600

    updated counter and added other elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import React from "react";
+import Counter from "./components/Counter";
 
 function App() {
-  return <div></div>;
+  return <div>
+
+    <Icon />
+    <Counter />
+
+  </div>;
 }
 
 export default App;
