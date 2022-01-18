9cf1f43a15315d555df5c8723ae0543fec8a5cad
Charnice <86922277+ctatum20@users.noreply.github.com>
2022-01-18 17:09:42 -0600

    added to counter

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,18 +1,99 @@
-import React from 'react'
+import React from "react";
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
+import "./Counter.css";
 
-function Counter() {
-    return (
-        <div>
-            <div id='clock'>
-                <p>Get ready! Phantom  has</p>
-                <p className='date'>{{ date }}</p>
-                <p className="time">{{ time }}</p>
-                <p className="text">UNTIL LAUNCH!</p>
-            </div>
-        </div>
-)}
+const minuteSeconds = 60;
+const hourSeconds = 3600;
+const daySeconds = 86400;
 
+const timerProps = {
+  isPlaying: true,
+  size: 120,
+  strokeWidth: 6,
+};
 
-export default Counter
+const renderTime = (dimension, time) => {
+  return (
+    <div className="time-wrapper">
+      <div className="time">{time}</div>
+      <div>{dimension}</div>
+    </div>
+  );
+};
 
+const getTimeSeconds = (time) => (minuteSeconds - time) | 0;
+const getTimeMinutes = (time) => ((time % hourSeconds) / minuteSeconds) | 0;
+const getTimeHours = (time) => ((time % daySeconds) / hourSeconds) | 0;
+const getTimeDays = (time) => (time / daySeconds) | 0;
 
+const Counter = () => {
+  const stratTime = Date.now() / 1000; // use UNIX timestamp in seconds
+  const endTime = stratTime + 243248; // use UNIX timestamp in seconds
+
+  const remainingTime = endTime - stratTime;
+  const days = Math.ceil(remainingTime / daySeconds);
+  const daysDuration = days * daySeconds;
+  return (
+    <div className="counter">
+      <CountdownCircleTimer
+        {...timerProps}
+        colors='#33cf12'
+        duration={daysDuration}
+        initialRemainingTime={remainingTime}
+      >
+        {({ elapsedTime, color }) => (
+          <span style={{ color }}>
+            {renderTime("days", getTimeDays(daysDuration - elapsedTime))}
+          </span>
+        )}
+      </CountdownCircleTimer>
+      <CountdownCircleTimer
+        {...timerProps}
+        colors="#33cf12"
+        duration={daySeconds}
+        initialRemainingTime={remainingTime % daySeconds}
+        onComplete={(totalElapsedTime) => ({
+          shouldRepeat: remainingTime - totalElapsedTime > hourSeconds,
+        })}
+      >
+        {({ elapsedTime, color }) => (
+          <span style={{ color }}>
+            {renderTime("hours", getTimeHours(daySeconds - elapsedTime))}
+          </span>
+        )}
+      </CountdownCircleTimer>
+      <CountdownCircleTimer
+        {...timerProps}
+        colors="#33cf12"
+        duration={hourSeconds}
+        initialRemainingTime={remainingTime % hourSeconds}
+        onComplete={(totalElapsedTime) => ({
+          shouldRepeat: remainingTime - totalElapsedTime > minuteSeconds,
+        })}
+      >
+        {({ elapsedTime, color }) => (
+          <span style={{ color }}>
+            {renderTime("minutes", getTimeMinutes(hourSeconds - elapsedTime))}
+          </span>
+        )}
+      </CountdownCircleTimer>
+      <CountdownCircleTimer
+        {...timerProps}
+        colors="#33cf12"
+        duration={minuteSeconds}
+        initialRemainingTime={remainingTime % minuteSeconds}
+        onComplete={(totalElapsedTime) => ({
+          shouldRepeat: remainingTime - totalElapsedTime > 0,
+        })}
+      >
+        {({ elapsedTime, color }) => (
+          <span style={{ color }}>
+            {renderTime("seconds", getTimeSeconds(elapsedTime))}
+          </span>
+        )}
+      </CountdownCircleTimer>
+    </div>
+  );
+};
+
+export default Counter;
