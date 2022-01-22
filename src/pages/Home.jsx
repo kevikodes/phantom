2f04045bc14cce38bfab46f537802ea209e699bb
Charnice <86922277+ctatum20@users.noreply.github.com>
2022-01-22 11:29:57 -0600

    changed font

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import Counter from "../components/Counter";
 import Mint from "../components/Mint";
 import "./home.css";
+import Email from "../components/Email";
 
 const Home = () => {
   return (
     <div className="home">
+       <div className="homeTitle">Mint your very own Phantom NFT!</div>
       <div><img src="images/ptm.png" alt="" /></div>
-      <div className="homeTitle">Mint your very own Phantom NFT!</div>
       {true && <Counter />}
       <div className="mintCard">{false && <Mint />}</div>
     </div>
