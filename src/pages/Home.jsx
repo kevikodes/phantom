adfe5c5e0195435afbe81302f2820956856319a4
Charnice <86922277+ctatum20@users.noreply.github.com>
2022-01-21 21:56:15 -0600

    added logo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import "./home.css";
 const Home = () => {
   return (
     <div className="home">
+      <div><img src="images/ptm.png" alt="" /></div>
       <div className="homeTitle">Mint your very own Phantom NFT!</div>
       {true && <Counter />}
       <div className="mintCard">{false && <Mint />}</div>
