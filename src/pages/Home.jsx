a1cd701a2b0169d042f3260837facba5fe862bd4
Kevin Owens III <krowens3rd@gmail.com>
2022-01-25 14:27:20 -0800

    up to date

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,27 @@ import React from "react";
 import Counter from "../components/Counter";
 import Mint from "../components/Mint";
 import "./home.css";
+import { useWeb3 } from "@3rdweb/hooks";
 
 const Home = () => {
+  const { address } = useWeb3();
   return (
     <div className="home">
       <div className="homeTitle">Mint your very own Phantom NFT!</div>
       {true && <Counter />}
-      <div className="mintCard">{true && <Mint />}</div>
+      <div className="mintCard">
+        {true && (
+          <>
+            <Mint />
+            <a
+              className="opensea-link"
+              href={`https://testnets.opensea.io/${address}`}
+            >
+              Check out your opensea to see your nft!
+            </a>
+          </>
+        )}
+      </div>
     </div>
   );
 };
