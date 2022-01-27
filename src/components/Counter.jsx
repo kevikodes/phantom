13795f670530b4b892dbd86fa96a0c6a981a2a70
Kevin Owens III <krowens3rd@gmail.com>
2022-01-27 09:27:23 -0800

    changed counter css import

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { CountdownCircleTimer } from "react-countdown-circle-timer";
 import "./Counter.css";
+//fixme: add timer props
 
 const minuteSeconds = 60;
 const hourSeconds = 3600;
