55d3245558531dda6929789d5a9fed30c801860b
Kevin Owens III <krowens3rd@gmail.com>
2022-01-27 13:25:29 -0800

    roadmap colors fixed

--- src/components/RoadmapCard.jsx
+++ src/components/RoadmapCard.jsx
@@ -3,71 +3,73 @@ import "./roadmap.css";
 
 const RoadmapCard = () => {
   return (
-    <div className="container darkGlassCard">
-      <div className="roadmap">
-        <div className="road-map">
-          <h1>Company Roadmap</h1>
+    <div className="roadmapWrapper">
+      <div className="container darkGlassCard">
+        <div className="roadmap">
+          <div className="road-map">
+            <h1>Company Roadmap</h1>
+          </div>
         </div>
+        <section className="timeline-wrapper">
+          <div className="middle-line"></div>
+
+          <div className="box box-top">
+            <div className="date"></div>
+            <div className="box-content">
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+              <p id="date1">Jul 1, 2021</p>
+            </div>
+          </div>
+
+          <div className="box box-bottom">
+            <div className="date"></div>
+            <div className="box-content">
+              <p>Lorem ipsum dolor sit amet, consectetur.</p>
+              <p id="date1">Jun 1, 2021</p>
+            </div>
+          </div>
+
+          <div className="box box-top">
+            <div className="date"></div>
+            <div className="box-content">
+              <p>Lorem ipsum dolor sit amet, consectetur.</p>
+              <p id="date1">May 1, 2021</p>
+            </div>
+          </div>
+
+          <div className="box box-bottom">
+            <div className="date"></div>
+            <div className="box-content">
+              <p id="date1">Apr 1, 2021</p>
+              <p>Lorem ipsum dolor sit amet, consectetur.</p>
+            </div>
+          </div>
+
+          <div className="box box-top">
+            <div className="date"></div>
+            <div className="box-content">
+              <p>Lorem ipsum dolor sit amet, consectetur.</p>
+              <p id="date1">Mar 1, 2021</p>
+            </div>
+          </div>
+
+          <div className="box box-bottom">
+            <div className="date"></div>
+            <div className="box-content">
+              <p id="date1">Feb 1, 2021</p>
+              <p>Lorem ipsum dolor sit amet, consectetur.</p>
+            </div>
+          </div>
+
+          <div className="box box-top">
+            <div className="date"></div>
+            <div className="box-content">
+              <p>Lorem ipsum dolor sit amet, consectetur.</p>
+              <p id="date1">Jan 1, 2021</p>
+            </div>
+          </div>
+        </section>
       </div>
-      <section className="timeline-wrapper">
-        <div className="middle-line"></div>
-
-        <div className="box box-top">
-          <div className="date"></div>
-          <div className="box-content">
-            <p>Lorem ipsum dolor sit amet consectetur.</p>
-            <p id="date1">Jul 1, 2021</p>
-          </div>
-        </div>
-
-        <div className="box box-bottom">
-          <div className="date"></div>
-          <div className="box-content">
-            <p>Lorem ipsum dolor sit amet, consectetur.</p>
-            <p id="date1">Jun 1, 2021</p>
-          </div>
-        </div>
-
-        <div className="box box-top">
-          <div className="date"></div>
-          <div className="box-content">
-            <p>Lorem ipsum dolor sit amet, consectetur.</p>
-            <p id="date1">May 1, 2021</p>
-          </div>
-        </div>
-
-        <div className="box box-bottom">
-          <div className="date"></div>
-          <div className="box-content">
-            <p id="date1">Apr 1, 2021</p>
-            <p>Lorem ipsum dolor sit amet, consectetur.</p>
-          </div>
-        </div>
-
-        <div className="box box-top">
-          <div className="date"></div>
-          <div className="box-content">
-            <p>Lorem ipsum dolor sit amet, consectetur.</p>
-            <p id="date1">Mar 1, 2021</p>
-          </div>
-        </div>
-
-        <div className="box box-bottom">
-          <div className="date"></div>
-          <div className="box-content">
-            <p id="date1">Feb 1, 2021</p>
-            <p>Lorem ipsum dolor sit amet, consectetur.</p>
-          </div>
-        </div>
-
-        <div className="box box-top">
-          <div className="date"></div>
-          <div className="box-content">
-            <p>Lorem ipsum dolor sit amet, consectetur.</p>
-            <p id="date1">Jan 1, 2021</p>
-          </div>
-        </div>
-      </section>
     </div>
   );
 };
